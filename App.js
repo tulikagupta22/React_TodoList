12942a9ebdad0e05d50291a0284665728519bf86
tulikagupta22 <76577676+tulikagupta22@users.noreply.github.com>
2024-06-03 12:50:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import { TodoWrapper } from './components/TodoWrapper';
+import './App.css';
+
+function App(){
+    return(
+        <div className="App">
+            <TodoWrapper/>
+  
+        </div>
+        
+    );
+}
+
+
+export default App;
+
